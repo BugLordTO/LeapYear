9e6705442b66fefd7f946293c28cc1f999ce951d
Somkiet <somsor@perfenterprise.com>
2018-11-03 12:15:22 +0700

    refactor and optimize test
    Signed-off-by: Somkiet <somsor@perfenterprise.com>

--- LeapYearCalculator.Test/LeapYearTest.cs
+++ LeapYearCalculator.Test/LeapYearTest.cs
@@ -8,8 +8,23 @@ namespace LeapYearCalculator.Test
     {
         [Theory]
         [InlineData(1, false)]
+        [InlineData(2, false)]
+        [InlineData(3, false)]
         [InlineData(4, true)]
+        [InlineData(5, false)]
+        [InlineData(6, false)]
+        [InlineData(7, false)]
+        [InlineData(8, true)]
+        [InlineData(9, false)]
+        [InlineData(10, false)]
+        [InlineData(20, true)]
+        [InlineData(18, false)]
+        [InlineData(99, false)]
         [InlineData(100, false)]
+        [InlineData(200, false)]
+        [InlineData(300, false)]
+        [InlineData(430, false)]
+        [InlineData(420, true)]
         [InlineData(1600, true)]
         [InlineData(1700, false)]
         [InlineData(1800, false)]
@@ -22,10 +37,10 @@ namespace LeapYearCalculator.Test
         [InlineData(2018, false)]
         [InlineData(2019, false)]
         [InlineData(2100, false)]
-        public void LeapYearCalculatorTest(int n, bool expected)
+        public void LeapYearCalculatorTest(int year, bool expected)
         {
             var sut = new LeapYear();
-            var actual = sut.Calculate(n);
+            var actual = sut.Calculate(year);
             actual.Should().Be(expected);
         }
     }

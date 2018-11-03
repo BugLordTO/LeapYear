fa4dd18f75a6df1a87c12fd9949d603dff06e9db
Somkiet <somsor@perfenterprise.com>
2018-11-03 12:10:00 +0700

    refactor TDD (mick)

--- LeapYearCalculator.Test/LeapYearTest.cs
+++ LeapYearCalculator.Test/LeapYearTest.cs
@@ -7,13 +7,22 @@ namespace LeapYearCalculator.Test
     public class LeapYearTest
     {
         [Theory]
-        [InlineData(1,false)]
-        [InlineData(2017, false)]
-        [InlineData(2019, false)]
+        [InlineData(1, false)]
         [InlineData(4, true)]
         [InlineData(100, false)]
         [InlineData(1600, true)]
-        public void LeapYearCalculatorTest(int n,bool expected)
+        [InlineData(1700, false)]
+        [InlineData(1800, false)]
+        [InlineData(1900, false)]
+        [InlineData(2000, true)]
+        [InlineData(2008, true)]
+        [InlineData(2012, true)]
+        [InlineData(2016, true)]
+        [InlineData(2017, false)]
+        [InlineData(2018, false)]
+        [InlineData(2019, false)]
+        [InlineData(2100, false)]
+        public void LeapYearCalculatorTest(int n, bool expected)
         {
             var sut = new LeapYear();
             var actual = sut.Calculate(n);

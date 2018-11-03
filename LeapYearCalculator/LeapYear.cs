0f76ec58f15de566e22dee34fa389a8140f3a941
Somkiet <somsor@perfenterprise.com>
2018-11-03 12:05:34 +0700

    Refactor code (nerd)

--- LeapYearCalculator/LeapYear.cs
+++ LeapYearCalculator/LeapYear.cs
@@ -4,11 +4,25 @@ namespace LeapYearCalculator
 {
     public class LeapYear
     {
+        private bool DivisibleBy4(int year)
+        {
+            return year % 4 == 0;
+        }
+
+        private bool DivisibleBy100(int year)
+        {
+            return year % 100 == 0;
+        }
+
+        private bool DivisibleBy400(int year)
+        {
+            return year % 400 == 0;
+        }
+
         public bool Calculate(int year)
         {
-            
-            if(year %100 == 0 && year% 400 != 0)return false;
-            else if (year % 400 == 0 || year % 4 == 0) return true;
+            if (DivisibleBy100(year) && !DivisibleBy400(year)) return false;
+            else if (DivisibleBy400(year) || DivisibleBy4(year)) return true;
             return false;
         }
     }
